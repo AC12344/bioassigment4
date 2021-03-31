11ea484d87cb8ab9deb046915ca17a2e2ce5726a
AC12344 <annec.poulsen@gmail.com>
2021-03-31 15:15:22 +0200

    added alligement, seperation and cohesion. wrap needs fixing

--- ultra_simple_sim.py
+++ ultra_simple_sim.py
@@ -10,6 +10,8 @@ NUMBER_OF_ROBOTS  = 50
 STEPS             = 500
 MAX_SPEED         = 0.1
 
+radius = 2
+
 # Positions
 x = np.random.uniform(low=0, high=ARENA_SIDE_LENGTH, size=(NUMBER_OF_ROBOTS,))
 y = np.random.uniform(low=0, high=ARENA_SIDE_LENGTH, size=(NUMBER_OF_ROBOTS,))
@@ -34,6 +36,10 @@ def init():
 
 def animate(i):
     global x, y, vx, vy
+    spe_fac = seperation()
+    cohe_fac = cohesion()
+    al_fac = alligement()
+    vx, vy = updateVel(spe_fac, cohe_fac,al_fac)
     x = np.array(list(map(wrap, x + vx)))
     y = np.array(list(map(wrap, y + vy)))
     
@@ -42,8 +48,109 @@ def animate(i):
     
     return points,
 
+def seperation():
+    global xs, ys, vys, vxs
+    dist = 0
+    seperation_fac = []
+    for i in range(NUMBER_OF_ROBOTS):
+        seperation_facx = 0
+        seperation_facy = 0
+        count = 1
+        for j in range(NUMBER_OF_ROBOTS):
+            dist = np.sqrt((x[i] - x[j])**2 + (y[i] - y[j])**2)
+            if i != j and dist < radius:
+                count += 1
+                if dist == 0:
+                    seperation_facx += MAX_SPEED
+                    seperation_facy += MAX_SPEED
+                else:
+                    seperation_facx += (x[i] - x[j])/dist
+                    seperation_facy += (y[i] - y[j])/dist
+
+        seperation_fac.append([seperation_facx/(count), seperation_facy/(count)])
+    return seperation_fac
+
+def cohesion():
+    cohesion_fac = []
+    dist = 0
+    for i in range(NUMBER_OF_ROBOTS):
+        cohesion_facx = 0
+        cohesion_facy = 0
+        count = 0
+        for j in range(NUMBER_OF_ROBOTS):
+            dist = np.sqrt((x[i] - x[j])**2 + (y[i] - y[j])**2)
+            #print(dist)
+            if dist < radius:
+                cohesion_facx += x[j]
+                cohesion_facy += y[j]
+                count +=1
+        
+        cohesion_fac.append([x[i]-cohesion_facx/count,y[i] - cohesion_facy/count])
+       # print("X : %f " % x[i])
+       # print("Y : %f " % y[i])
+       # print(cohesion_fac[i])
+    return cohesion_fac
+
+def alligement():
+    al_fac = []
+    
+    for i in range(NUMBER_OF_ROBOTS):
+        al_facx = 0
+        al_facy = 0
+        count = 0
+        for j in range(NUMBER_OF_ROBOTS):
+            dist = np.sqrt((x[i] - x[j])**2 + (y[i] - y[j])**2)
+            if dist < radius:
+                al_facx += vx[j]
+                al_facy += vy[j]
+                count += 1
+        al_fac.append([al_facx/count,al_facy/count])
+    return al_fac
+
+def updateVel(spe_fac, coh_fac,al_fac):
+    
+    vx_s = np.array(spe_fac)[:,0]
+    vy_s = np.array(spe_fac)[:,1]
+    vx_s_max = np.max(np.linalg.norm(spe_fac))
+    vy_s_max = np.max(np.linalg.norm(spe_fac))
+    if vx_s_max != 0:
+        vx_s = vx_s/vx_s_max*MAX_SPEED
+    else:
+        vx_s = 0.1*MAX_SPEED
+    if vy_s_max != 0:
+        vy_s = vy_s / vy_s_max * MAX_SPEED
+    else:
+        vy_s = 0.1*MAX_SPEED
+    vx_c = np.array(coh_fac)[:,0]
+    vy_c = np.array(coh_fac)[:,1]
+    vx_c_max = np.max(np.linalg.norm(coh_fac))
+    vy_c_max = np.max(np.linalg.norm(coh_fac))
+    if vy_c_max != 0:
+        vx_c = vx_c/vx_c_max*MAX_SPEED
+    else:
+        vx_c = vx_c*MAX_SPEED
+    if vy_c_max != 0:
+        vy_c = vy_c/vy_c_max*MAX_SPEED
+    else:
+        vy_c = vy_c*MAX_SPEED
+    vx_a = np.array(al_fac)[:,0]
+    vy_a = np.array(al_fac)[:,1]
+    vx_a_max = np.max(np.linalg.norm(al_fac))
+    vy_a_max = np.max(np.linalg.norm(al_fac))
+    if vx_a_max != 0:
+        vx_a = vx_a/vx_a_max*MAX_SPEED
+    else:
+        vx_a = vx_a*MAX_SPEED
+    if vy_a_max != 0:
+        vy_a = vy_a/vy_a_max*MAX_SPEED
+    else:
+        vy_a = vy_a*MAX_SPEED
+    vx = 1*vx_s + 1*vx_c + 2*vx_a 
+    vy = 1*vy_s + 1*vy_c + 2*vy_a
+    
+    return vx, vy
+
 anim = FuncAnimation(fig, animate, init_func=init,
                                frames=STEPS, interval=1, blit=True)
-
 writervideo = animation.FFMpegWriter(fps=60)
 anim.save("output.mp4", writer=writervideo)
